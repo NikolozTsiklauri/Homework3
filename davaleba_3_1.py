a47b8325c0d82505a86a1d11f118163d94023b89
Nikoloz Tsiklauri <101805759+NikolozTsiklauri@users.noreply.github.com>
2022-04-08 13:45:17 +0400

    Update davaleba_3_1.py

--- davaleba_3_1.py
+++ davaleba_3_1.py
@@ -4,10 +4,10 @@ from botocore.exceptions import ClientError
 s3 = boto3.client('s3')
 
 
-def upload_file(input_name, bucket_name, output_name):
+def upload_file(input_name, name, output_name):
     with open(input_name, 'rb') as file:
         try:
-            s3.upload_fileobj(file, bucket_name, output_name)
+            s3.upload_fileobj(file, name, output_name)
         except ClientError as e:
             print(e)
             return
